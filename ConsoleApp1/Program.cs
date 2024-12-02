8fac16e0edb971654f6f9f847e21d308f475afa7
stas115gr <stashauzkin10@gmail.com>
2024-12-02 20:35:07 +0200

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = Vvod("Кол-во метров ");
+            int c = Vvod("Кол-во сантиметров ");
+            Add("Кол-во сантиметров=", Centimeters(m, c));
+            Console.ReadKey();
+        }
+        public static int Centimeters(int m, int c)
+        {
+            return m * 100 + c;
+        }
+        public static int Vvod(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static void Add(string message, int m)
+        {
+            Console.WriteLine($"{message} {m}");
+        }
+    }
+}
