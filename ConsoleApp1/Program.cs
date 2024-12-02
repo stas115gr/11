015f3e1c9af92706511f603fd75a8454cf78ed4a
stas115gr <stashauzkin10@gmail.com>
2024-12-02 20:51:06 +0200

    task4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = Vvod("a=");
+            int number2 = Vvod("b=");
+            Console.WriteLine($"Z = {Func(2 * number1, number2 + number1) + Func(2 * number1 - number2, number2)}");
+            Console.ReadLine();
+        }
+        public static int Vvod(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static int Func(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+    }
+}
